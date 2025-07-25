52a2e04ac02299dd111b3afbc897d11eb9eae0bb
Chris99-j <your.email@example.com>
2025-07-25 11:34:10 +0800

    Setup mini-bank deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/mini-bank/', // ✅ Must match your GitHub repo name
-  plugins: [vue()]
+  plugins: [vue()],
 })
+// 
