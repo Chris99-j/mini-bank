0dedbb751c79212463baf731ac48b1fb2b4d0325
Chris99-j <your.email@example.com>
2025-07-25 10:44:14 +0800

    Add missing folders and components

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/bank-system/',  // must match your GitHub repo name
+  base: '/mini-bank/', // ✅ Must match your GitHub repo name
   plugins: [vue()]
 })
